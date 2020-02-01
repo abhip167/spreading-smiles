6a148abe4da6306250c737fcac933e6338dd6f66
Abhishek Patel <abhishekpatel167@gmail.com>
2020-02-01 14:35:55 +0530

    WEBP LOADED

--- png-jpg.js
+++ png-jpg.js
@@ -1,11 +1,23 @@
 
 const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
-PNGImages = "./static/assets/swacchta/**/**.jpg",         // PNG images       // JPEG images
+SwacchtaImages = "./static/assets/swacchta/**/**.jpg",         // PNG images       // JPEG images
+EducationImages = "./static/assets/education/**/**.jpg",         // PNG images       // JPEG images
+HealthImages = "./static/assets/health/**/**.jpg",         // PNG images       // JPEG images
 
 (async () => {
-  const files = await imagemin([PNGImages], {
+  const files = await imagemin([SwacchtaImages], {
     destination: './static/webp/swacchta',
+    plugins: [imageminWebp()],
+  });
+
+  const filesEdu = await imagemin([EducationImages], {
+    destination: './static/webp/education',
+    plugins: [imageminWebp()],
+  });
+
+  const filesHealth = await imagemin([HealthImages], {
+    destination: './static/webp/health',
     plugins: [imageminWebp()],
   });
 
@@ -13,5 +25,5 @@ PNGImages = "./static/assets/swacchta/**/**.jpg",         // PNG images       //
 
 
 
-  console.log(JPEGiles);
+  console.log(SwacchtaImages);
 })();
